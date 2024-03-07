888dca0fb49a165a99d39317c6d8ed45db50bdda
ronaldmelara <ronaldmelara@gmail.com>
2024-03-07 02:00:10 -0300

    Se agrega método comentado. Este método se utilizó para leer el archivo JSON, deserializarlo y guardar los registros en la BD utilizando EntityFramework

--- Consalud.DataAccess/Repositories/FacturasDA.cs
+++ Consalud.DataAccess/Repositories/FacturasDA.cs
@@ -113,6 +113,30 @@ namespace Consalud.DataAccess.Repositories
             return Data.Facturas.Include(factura => factura.DetalleFactura).ThenInclude(detalle => detalle.Producto).ToList(); 
         }
 
+
+        //Método utilizado para hacer el bulk de datos del JSON de ejemplo a last tablas del SQLite
+        //private void BulkData()
+        //{
+            //        var jsonString = File.ReadAllText("JsonEjemplo.json");
+            //        var facturas = JsonSerializer.Deserialize<List<Facturas>>(jsonString);
+
+
+
+            //        foreach (var factura in facturas)
+            //        {
+            //              db.Data.Facturas.Add(factura);
+
+            //	            foreach (var detalleFactura in factura.DetalleFactura)
+            //	            {
+            //		            // Guarda el detalle de la factura en la base de datos
+            //		            db.Data.DetalleFactura.Add(detalleFactura);
+
+            //		            // Guarda el producto asociado con el detalle de la factura en la base de datos
+            //		            db.Data.Producto.Add(detalleFactura.Producto);
+            //	            }
+            //         }
+            //         db.Data.SaveChanges();
+        //}
     }
 }
 
